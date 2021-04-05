82a1888098287b29b9da16853f1e328bbb824925
Your Name <Developer1545>
2021-04-05 18:41:10 -0400

    Event handlers
    Added the last two event handlers for open to the blog posts

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 $('#economy').click(function(){
     window.open('pages/ecomomy.html');
 });
+
+$('#guadalcanal').click(function(){
+    window.open('pages/guadalcanal.html');
+});
+
+$('#pearlharbor').click(function(){
+    window.open('pages/pearlharbor.html');
+})
